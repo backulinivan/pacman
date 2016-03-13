105549097425835f66644d99acf2ff4936e3301b
backulinivan <backulinivan@gmail.com>
2016-03-13 21:38:43 +0300

    so much pain

--- pacman.py
+++ pacman.py
@@ -55,54 +55,75 @@ class Ghost(GameObject):
         if self.tick % 20 == 0 or self.direction == 0:
             self.direction = random.randint(1, 4)
 
-        if self.direction == 1:
-            self.x += self.velocity
-            if self.x >= self.map_size-1:
-                self.x = self.map_size-1
-                self.direction = random.randint(1, 4)
-        elif self.direction == 2:
-            self.y += self.velocity
-            if self.y >= self.map_size-1:
-                self.y = self.map_size-1
-                self.direction = random.randint(1, 4)
-        elif self.direction == 3:
-            self.x -= self.velocity
-            if self.x <= 0:
-                self.x = 0
-                self.direction = random.randint(1, 4)
-        elif self.direction == 4:
-            self.y -= self.velocity
-            if self.y <= 0:
-                self.y = 0
-                self.direction = random.randint(1, 4)
-        self.set_coord(self.x, self.y)
-
-
-class Pacman(GameObject):
-    def __init__(self, x, y, tile_size, map_size):
-        GameObject.__init__(self, './resources/pacman.png', x, y, tile_size, map_size)
-        self.direction = 0
-        self.velocity = 4.0 / 10.0
-
-
-    def game_tick(self, _map):
-        super(Pacman, self).game_tick(_map)
         if self.direction == 1 and not Map.is_wall(_map, int(self.x+1), int(self.y)):
             self.x += self.velocity
             if self.x >= self.map_size-1:
                 self.x = self.map_size-1
+                self.direction = random.choice((2, 3, 4))
         elif self.direction == 2 and not Map.is_wall(_map, int(self.x), int(self.y+1)):
             self.y += self.velocity
             if self.y >= self.map_size-1:
                 self.y = self.map_size-1
+                self.direction = random.choice((1, 3, 4))
         elif self.direction == 3 and not Map.is_wall(_map, int(self.x-1), int(self.y)):
             self.x -= self.velocity
             if self.x <= 0:
                 self.x = 0
+                self.direction = random.choice((1, 2, 4))
         elif self.direction == 4 and not Map.is_wall(_map, int(self.x), int(self.y-1)):
             self.y -= self.velocity
             if self.y <= 0:
                 self.y = 0
+                self.direction = random.choice((1, 2, 3))
+        self.set_coord(self.x, self.y)
+
+
+
+class Dot(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, './resources/food.png', x, y, tile_size, map_size)
+
+
+class Pacman(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, './resources/pacman_right.png', x, y, tile_size, map_size)
+        self.direction = 0
+        self.velocity = 5.0 / 10.0
+
+
+    def game_tick(self, _map):
+        super(Pacman, self).game_tick(_map)
+
+        if self.direction == 1 and Map.is_dot(_map, int(self.x+1), int(self.y)):
+            Map.remove_object(_map, int(self.x+1), int(self.y))
+        elif self.direction == 2 and Map.is_dot(_map, int(self.x), int(self.y+1)):
+            Map.remove_object(_map, int(self.x), int(self.y+1))
+        elif self.direction == 3 and Map.is_dot(_map, int(self.x-1), int(self.y)):
+            Map.remove_object(_map, int(self.x-1), int(self.y))
+        elif self.direction == 4 and Map.is_dot(_map, int(self.x), int(self.y-1)):
+            Map.remove_object(_map, int(self.x), int(self.y-1))
+
+        if self.direction == 1 and not Map.is_wall(_map, int(self.x+1), int(self.y)):
+            self.image = pygame.image.load('./resources/pacman_right.png')
+            self.x += self.velocity
+            if self.x >= self.map_size-1:
+                self.x = self.map_size-1
+        elif self.direction == 2 and not Map.is_wall(_map, int(self.x), int(self.y+1)):
+            self.image = pygame.image.load('./resources/pacman_down.png')
+            self.y += self.velocity
+            if self.y >= self.map_size-1:
+                self.y = self.map_size-1
+        elif self.direction == 3 and not Map.is_wall(_map, int(self.x-1), int(self.y)):
+            self.image = pygame.image.load('./resources/pacman_left.png')
+            self.x -= self.velocity
+            if self.x <= 0:
+                self.x = 0
+        elif self.direction == 4 and not Map.is_wall(_map, int(self.x), int(self.y-1)):
+            self.image = pygame.image.load('./resources/pacman_up.png')
+            self.y -= self.velocity
+            if self.y <= 0:
+                self.y = 0
+
 
         self.set_coord(self.x, self.y)
 
@@ -120,16 +141,26 @@ class Map:
             for (x, c) in enumerate(l):
                 if txt_map[y][x] == '#':
                     self.map[y][x] = Wall(x, y, tile_size, map_size)
+                elif txt_map[y][x] == '.':
+                    self.map[y][x] = Dot(x, y, tile_size, map_size)
 
     def draw_map(self):
         for y in range(len(self.map)):
             for x in range(len(self.map[y])):
-                if type(self.map[y][x]) == Wall:
+                if type(self.map[y][x]) == Wall or type(self.map[y][x]) == Dot:
                     self.map[y][x].draw(screen)
 
     def is_wall(self, x, y):
         if y < len(self.map) and x < len(self.map[y]):
             return type(self.map[y][x]) == Wall
+
+    def is_dot(self, x, y):
+        if y < len(self.map) and x < len(self.map[y]):
+            return type(self.map[y][x]) == Dot
+
+    def remove_object(self, x, y):
+        self.map[y][x] = None
+
 
 
 
@@ -154,7 +185,10 @@ if __name__ == '__main__':
     init_window()
     tile_size = 32
     map_size = 16
-    ghost = Ghost(0, 0, tile_size, map_size)
+    ghost1 = Ghost(0, 5, tile_size, map_size)
+    ghost2 = Ghost(10, 10, tile_size, map_size)
+    ghost3 = Ghost(8, 8, tile_size, map_size)
+    ghost4 = Ghost(9, 9, tile_size, map_size)
     pacman = Pacman(5, 5, tile_size, map_size)
     background = pygame.image.load("./resources/background.png")
     screen = pygame.display.get_surface()
@@ -163,10 +197,16 @@ if __name__ == '__main__':
     while 1:
         process_events(pygame.event.get(), pacman)
         pygame.time.delay(100)
-        ghost.game_tick(map)
+        ghost1.game_tick(map)
+        ghost2.game_tick(map)
+        ghost3.game_tick(map)
+        ghost4.game_tick(map)
         pacman.game_tick(map)
         draw_background(screen, background)
         pacman.draw(screen)
-        ghost.draw(screen)
+        ghost1.draw(screen)
+        ghost2.draw(screen)
+        ghost3.draw(screen)
+        ghost4.draw(screen)
         map.draw_map()
         pygame.display.update()
